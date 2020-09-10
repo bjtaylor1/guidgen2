9fce859390a2b9a00ee0c9a8bf6bcead1a35915d
Ben Taylor <bentaylortheonly@gmail.com>
2020-09-10 12:25:51 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace guidgen2
+{
+    class Program
+    {
+        [STAThread]
+        static async Task Main(string[] args)
+        {
+            var guid = Guid.NewGuid();
+            var guidString = guid.ToString();
+            await TextCopy.ClipboardService.SetTextAsync(guidString);
+            await Console.Out.WriteLineAsync(guidString);
+        }
+    }
+}
